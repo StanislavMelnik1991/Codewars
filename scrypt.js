8a858196ed7785cd88c1c41d3de476d5019c113e
Stanislav Melnik <stanislavmelnik@gmail.com>
2022-03-15 17:39:22 +0300

    feat: add input and button

--- scrypt.js
+++ scrypt.js
@@ -1,6 +1,14 @@
 function getParticipants(handshakes){
-
-  return Math.ceil((1+(1+(4*2*handshakes))**(1/2))/2)
-  // ...
+  return Math.ceil((1+(1+(4*2*handshakes))**(1/2))/2);
 }
-console.log(getParticipants(6))
+
+butt.onclick = function() {
+  let val = document.getElementById('elem1').value;
+  let people = getParticipants(val);
+  document.getElementById('str').innerHTML="Людей необходимо: "+people;
+
+
+};
+
+
+//console.log(getParticipants(6))
