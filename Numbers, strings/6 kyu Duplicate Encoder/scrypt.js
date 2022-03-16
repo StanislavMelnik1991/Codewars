5e4817cf83f8c13d1f499f51cf63a0c0d3cad867
Stanislav Melnik <stanislavmelnik@gmail.com>
2022-03-16 22:07:28 +0300

    feat: create basic layout for task

---
+++ Numbers, strings/6 kyu Duplicate Encoder/scrypt.js
@@ -0,0 +1,15 @@
+function duplicateEncode(word){
+    // ...
+
+
+    
+    return 1
+}
+  
+  butt.onclick = function() {
+    let val = document.getElementById('elem1').value;
+    //
+    let output = duplicateEncode(val);
+    //
+    document.getElementById('str').innerHTML=output;
+  };
