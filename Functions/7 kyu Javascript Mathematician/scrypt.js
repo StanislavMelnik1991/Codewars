55a4006ee9a5013e4d96da2125f45209eca1f045
Stanislav Melnik <stanislavmelnik@gmail.com>
2022-03-21 16:05:56 +0300

    feat: add styles to all pages

---
+++ Functions/7 kyu Javascript Mathematician/scrypt.js
@@ -0,0 +1,15 @@
+function calculate() {
+let result
+  return result
+}
+  butt.onclick = function() {
+    let val = document.getElementById('elem1').value;
+    //
+    let arr = val.split(', ');
+    let output = sortByBit(arr); 
+    //
+    document.getElementById('str').innerHTML=output;
+  };
+
+
+  
