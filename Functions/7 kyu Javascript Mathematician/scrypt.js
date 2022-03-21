bdaa16788e2494df6dac84c1c684fa9fff62c6e7
Stanislav Melnik <stanislavmelnik@gmail.com>
2022-03-21 18:44:34 +0300

    feat: task 7 kyu Javascript Mathematician complete

--- Functions/7 kyu Javascript Mathematician/scrypt.js
+++ Functions/7 kyu Javascript Mathematician/scrypt.js
@@ -1,12 +1,26 @@
+let result = 0
 function calculate() {
-let result
-  return result
+let args1 = [...calculate.arguments];
+ console.log(args1 + "     first")  
+  function inside() {
+ let args2 = [...inside.arguments];
+    result = args2.reduce((previous, current) => {
+return (previous + current)});
+    result += args1.reduce((previous, current) => {
+return (previous + current)});
+ console.log(args2 + "     second")  
+  console.log(result + "     result")     
+    return result;
+  }
+  return inside;
 }
+
+
+console.log(calculate(2,4)(3,7,1));
   butt.onclick = function() {
     let val = document.getElementById('elem1').value;
     //
-    let arr = val.split(', ');
-    let output = sortByBit(arr); 
+    let output = calculate(val); 
     //
     document.getElementById('str').innerHTML=output;
   };
