b1b01effc12817517646c6b0ad2153b539bb866f
Stanislav Melnik <stanislavmelnik@gmail.com>
2022-03-21 15:48:41 +0300

    feat: task 6 kyu Sorting by bits complete

--- Arrays/6 kyu Sorting by bits/scrypt.js
+++ Arrays/6 kyu Sorting by bits/scrypt.js
@@ -1,12 +1,27 @@
 function sortByBit(arr) {
   // your solution here
   let result = arr
+
+result.sort((a, b) => (b-a));
   for (let i = 0; (i < result.length); i++) {
-result[i] = +parseInt(result[i]).toString(2);
+result[i] = parseInt(result[i]).toString(2);
   }
 
-  
-console.log(result);
+//result.sort((a, b) => (a.split('').reduce((a1,b1) => (parseInt(a1)+parseInt(b1)))) - (b.split('').reduce((a1,b1) => (parseInt(a1)+parseInt(b1)))));
+result.sort(function(a,b) {
+if ((a.split('').reduce((a1,b1) => (parseInt(a1)+parseInt(b1)))) == (b.split('').reduce((a1,b1) => (parseInt(a1)+parseInt(b1))))) {
+  return (a - b);
+}
+else {
+  return ((a.split('').reduce((a1,b1) => (parseInt(a1)+parseInt(b1)))) - (b.split('').reduce((a1,b1) => (parseInt(a1)+parseInt(b1)))));
+}
+
+
+})
+console.log(arr);
+for (let i = 0; (i < result.length); i++) {
+  result[i] = parseInt(result[i],2);
+    }
 console.log(arr);
 return result
 }
