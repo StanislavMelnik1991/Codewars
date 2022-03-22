9d1a2ee6ef1d28c28184a93738137073803ab886
Stanislav Melnik <stanislavmelnik@gmail.com>
2022-03-22 20:21:11 +0300

    feat: task 6 kyu Can you keep a secret initiated

---
+++ Functions/6 kyu Can you keep a secret/scrypt.js
@@ -0,0 +1,17 @@
+function createSecretHolder(secret) {
+
+
+  }
+
+
+console.log(calculate(2,4)(3,7,1));
+  butt.onclick = function() {
+    let val = document.getElementById('elem1').value;
+    //
+    let output = calculate(val); 
+    //
+    document.getElementById('str').innerHTML=output;
+  };
+
+
+  
