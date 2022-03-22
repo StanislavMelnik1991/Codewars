c59d855cfa902750f68ed52aafb21b36c41d3185
Stanislav Melnik <stanislavmelnik@gmail.com>
2022-03-22 20:49:46 +0300

    feat: task 6 kyu Can you keep a secret complete

--- Functions/6 kyu Can you keep a secret/scrypt.js
+++ Functions/6 kyu Can you keep a secret/scrypt.js
@@ -1,8 +1,15 @@
 function createSecretHolder(secret) {
-
-
+  let obj = {
+    result: secret,
+    getSecret() { 
+      return (this.result);
+    },
+      setSecret(tmp) { 
+      this.result = tmp;
+    }
+  };
+  return obj
   }
-
 
 console.log(calculate(2,4)(3,7,1));
   butt.onclick = function() {
